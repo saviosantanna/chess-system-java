4c3b6ac1a930fa3fc82b4dc7fe1712b8aacf69ab
Sávio Sant'anna <saviosantannadesa@gmail.com>
2023-03-10 10:18:45 -0300

    Placing pieces on the board e correcao codigo

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,14 +2,13 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
-import boardgame.Position;
 
 public class ChessPiece extends Piece {
 
 	private Color color;
 
-	public ChessPiece(Position postion, Board board, Color color) {
-		super(postion, board);
+	public ChessPiece(Board board, Color color) {
+		super(board);
 		this.color = color;
 	}
 

d6cfdf3a5afa11461a2edad05ba066f3bf0c6106
Sávio Sant'anna <saviosantannadesa@gmail.com>
2023-08-23 17:51:40 -0300

    Fix - better treatment to promotion and correction gitgnore

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.nio.channels.IllegalSelectorException;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
@@ -124,7 +123,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no pieces to prmoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
